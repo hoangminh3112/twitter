3d86f87b8e183ba001d803e9f5ff108251d98440
Phi Nguyen Hai Minh <minhpnh.bi10-120@st.usth.edu.vn>
2021-11-09 22:15:56 +0700

    Tweet Upload

---
+++ app/src/main/java/vn/edu/usth/twitter/TweetUpload.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.twitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TweetUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_upload);
+    }
+}
